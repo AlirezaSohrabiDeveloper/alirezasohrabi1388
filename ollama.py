b421fb190e19fe470f8e6066f7ea6547e232e81d
Alireza <127936454+AlirezaSohrabiDeveloper@users.noreply.github.com>
2026-05-28 15:58:17 +0330

    Update ollama.py

--- ollama.py
+++ ollama.py
@@ -1,14 +1,1 @@
-from langchain_community.llms import Ollama
 
-llm = Ollama(model='llama3.2')
-
-x=2
-
-while x < 3:
-
-    inp = input('talk to me : ')
-
-    responce = llm.invoke(inp)
-
-    print(responce)
-    
